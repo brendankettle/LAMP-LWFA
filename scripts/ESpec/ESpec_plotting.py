e697f3e08aa101d59255a889b8d4915e4e4a79dd
Brendan Kettle <38698119+brendankettle@users.noreply.github.com>
2024-09-12 15:40:45 +0300

    Create ESpec_plotting.py

---
+++ scripts/ESpec/ESpec_plotting.py
@@ -0,0 +1,23 @@
+# define root folder and import LAMP
+import sys, os
+ROOT_FOLDER = os.path.dirname(__file__) + '/../../' # point this to the folder containing LAMP and/or the config files
+sys.path.append(ROOT_FOLDER)
+from LAMP import Experiment
+# -----------------------------------------------------------------------------
+import matplotlib.pyplot as plt 
+import numpy as np
+
+# create experiment object
+ex = Experiment(ROOT_FOLDER)
+
+# get ESpec diagnostic
+ESpec = ex.get_diagnostic('ESpec')
+
+# define a shot
+eg_shot = {'date': 20241225, 'run': 'run01', 'shotnum': 1}
+
+# plot
+fig, ax = ESpecHigh.plot_proc_shot(eg_shot, debug=True)
+#ax.set_xlim([100,2000])
+#ax.set_ylim([-50,50])
+plt.tight_layout()
